dd3d8753b0eeb60c2996a756bdd63e4ad3d8052c
RavenHex1296 <suanoo5196@gmail.com>
2022-08-24 18:56:20 +0000

    fixed dictionaries actually

--- dictionaries.py
+++ dictionaries.py
@@ -10,12 +10,24 @@ def howPoints(player):
 
 
 def cheater(player):
-    points[players] = 0
+    points[player] = 0
 
 
 def endGame():
+    max_point_player = list(points.keys())[0]
+
     for key in points:
         print(key + " has " + str(points[key]) + " points.")
 
+        if points[key] > points[max_point_player]:
+            max_point_player = key
 
+    print(max_point_player + " has won!!!")
+
+
+addPoints("Justin", 727)
+addPoints("Xida", 20)
+addPoints("Andrew", 45)
+addPoints("Angel", 42)
+cheater("Justin")
 endGame()
