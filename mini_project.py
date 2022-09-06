462a929981eae15798d4653dad4d809bbf098fed
RavenHex1296 <suanoo5196@gmail.com>
2022-09-06 03:12:35 +0000

    fixed mini_project

--- mini_project.py
+++ mini_project.py
@@ -1,3 +1,4 @@
+input_planet = input("Which planet do you want to know your age on? Options: Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto ")
 input_age = input("How old are you? ")
 
 num_earth_days = float(input_age) * 365
@@ -7,5 +8,4 @@ conversion_to_planet_factors = {"Mercury": 87.97, "Venus": 225, "Mars": 687 , "J
                                 "Saturn": 10585, "Uranus": 30660, "Neptune": 60225, "Pluto": 90520}
 
 
-for planet in conversion_to_planet_factors:
-    print("You are " + str(round(num_earth_days / conversion_to_planet_factors[planet], 2)) + " years old on " + planet + ".")
+print("You are " + str(round(num_earth_days / conversion_to_planet_factors[input_planet], 2)) + " years old on " + input_planet + ".")
