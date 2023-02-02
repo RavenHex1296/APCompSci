872625031c779985020c91b33289a868dfaa0b11
RavenHex1296 <suanoo5196@gmail.com>
2023-02-02 19:54:16 +0000

    done with dna encrpytion stuff

--- encryption_project/mini_project.py
+++ encryption_project/mini_project.py
@@ -1,5 +1,11 @@
 rule1 = ['C', 'T', 'A', 'G']
-
+rule2 = ['C', 'A', 'T', 'G']
+rule3 = ['G', 'A', 'T', 'C']
+rule4 = ['G', 'T', 'A', 'C']
+rule5 = ['A', 'G', 'C', 'T']
+rule6 = ['A', 'C', 'G', 'T']
+rule7 = ['T', 'C', 'G', 'A']
+rule8 = ['T', 'G', 'C', 'A']
 
 def xor(bit1, bit2):
     if bit1 == bit2:
@@ -8,29 +14,29 @@ def xor(bit1, bit2):
     return "1"
 
 
-def encodeDNA(character):
-    charIndex = rule1.index(character)
+def encodeDNA(character, rule):
+    charIndex = rule.index(character)
     return '{0:02b}'.format(charIndex)
 
 
-def decodeDNA(binary):
+def decodeDNA(binary, rule):
     charIndex = int(binary, 2)
-    return rule1[charIndex]
+    return rule[charIndex]
 
 
-def convert_DNA_to_binary(DNA):
+def convert_DNA_to_binary(DNA, rule):
     binary_DNA = ""
 
     for n in range(len(DNA)):
-        binary_DNA += encodeDNA(DNA[n])
+        binary_DNA += encodeDNA(DNA[n], rule)
 
     return binary_DNA
 
 
 # finish the following function
-def XOR_DNA(plainDNA, keyDNA):
-    plainDNA_number = convert_DNA_to_binary(plainDNA)
-    keyDNA_number = convert_DNA_to_binary(keyDNA)
+def XOR_DNA(plainDNA, keyDNA, rule):
+    plainDNA_number = convert_DNA_to_binary(plainDNA, rule)
+    keyDNA_number = convert_DNA_to_binary(keyDNA, rule)
 
     encoded_dna_num = ""
 
@@ -40,7 +46,7 @@ def XOR_DNA(plainDNA, keyDNA):
     encoded_dna = ""
 
     for n in range(0, len(encoded_dna_num), 2) :
-        encoded_dna += decodeDNA(encoded_dna_num[n] + encoded_dna_num[n + 1])
+        encoded_dna += decodeDNA(encoded_dna_num[n] + encoded_dna_num[n + 1], rule)
 
     # return the encoded DNA based on the key and rule   
     return encoded_dna
@@ -52,7 +58,7 @@ def XOR_DNA(plainDNA, keyDNA):
 
 # MODIFY THE METHODS SO THAT USERS CAN CHOOSE FROM THE EIGHT POSSIBLE DNA RULES
 # TRY TO FIGURE OUT THE RULES YOURSELF FIRST! IF YOU GET STUCK COME ASK ME :)
-# REMEMBER WATSON CRICK SAYS A PAIRS WITH T & G PAIRS WITH
-assert XOR_DNA("ACGT", "CGTA") == "AGAG" 
-assert XOR_DNA("AGAG", "CGTA") == "ACGT"
+# REMEMBER WATSON CRICK SAYS A PAIRS WITH T & G PAIRS WITH C
+assert XOR_DNA("ACGT", "CGTA", rule1) == "AGAG" 
+assert XOR_DNA("AGAG", "CGTA", rule1) == "ACGT"
 print("PASSED")
